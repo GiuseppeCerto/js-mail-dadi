5b6da3a642729857fcbc0dda52f35c2bbf653005
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-03 14:14:10 +0100

    START mail dadi

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// creare un array con una lista di mail
+// creare un prompt che chiede una email all utente
+// salvare il valore del prompt denrto una variabile
+// aggiungere l' email al array 
+// cotrollare se l'email e' presente nell array
+// stampare un messaggio nel DOM con lésito del controllo
+
+
+
+
+// generare un numero random per l' utente
+// generare un numero random per l' il computer
+// stabilire chi vince in base al numero piu alto
