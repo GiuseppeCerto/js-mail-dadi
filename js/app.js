99d7b92c3963323182532a56b6f9fddc6abf0532
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-03 15:10:13 +0100

    EDIT email confirm

--- js/app.js
+++ js/app.js
@@ -1,9 +1,42 @@
 // creare un array con una lista di mail
+
+const emailList = ['rus3085@gmailni.com', 'belyanskajai@cashbackr.com', 'ewanmc@turuwae.tech', 'en555@enhanceronly.com', 'rooktoqueen@scatterteam.com',]
+
+console.log(emailList)
+console.log(emailList.length)
+
 // creare un prompt che chiede una email all utente
-// salvare il valore del prompt denrto una variabile
-// aggiungere l' email al array 
+
+const newMailUtent= document.getElementById('emailfromutent')
+
+// salvare il valore del prompt dentro una variabile
+
+const btnConfirm = document.getElementById('buttonconfirm')
+
+btnConfirm.addEventListener('click', function() {
+
+const mailUtent = newMailUtent.value
+console.log(mailUtent)
+
+// aggiungere l' email al array
+
+emailList.push(mailUtent)
+
 // cotrollare se l'email e' presente nell array
-// stampare un messaggio nel DOM con lésito del controllo
+
+console.log(emailList)
+
+let emailConfirm = document.getElementById('nuovaemail')
+
+// stampare un messaggio nel DOM con l'esito del controllo
+
+emailConfirm.innerHTML = 'La tua email: '+ mailUtent + ' è stata confermata'
+
+})
+
+
+
+
 
 
 
@@ -11,3 +44,4 @@
 // generare un numero random per l' utente
 // generare un numero random per l' il computer
 // stabilire chi vince in base al numero piu alto
+
